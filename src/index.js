bc06f53ee90c2779c712ef7e0b51cfa9aa8054e0
Yonipy <itz.kqlvin@gmail.com>
2022-07-23 13:55:21 -0400

    eeee

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import './css/navbar.css'
 import './css/page.css'
-import './css/footer.css'
 import Navbar from './componets/Main Page/navbar';
 import Page from './componets/Main Page/page';
-import Footer from './componets/Main Page/footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Navbar />
     <Page />
-    <Footer />
   </React.StrictMode>
 );
 
